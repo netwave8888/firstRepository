8fafd00b497c6ba647ae5844d446e04c0fe3cf2c
Tao Xu <taoxu8888@gmail.com>
2020-10-29 23:05:47 -0400

    exceptions

---
+++ jss2/exceptions/EmptySetException.java
@@ -0,0 +1,24 @@
+//********************************************************************
+//  EmptySetException.java     Authors: 
+//
+//  Represents the situation in which a set is empty.
+//********************************************************************
+
+package cardgame.jss2.exceptions;
+
+public class EmptySetException extends RuntimeException
+{
+   //-----------------------------------------------------------------
+   //-----------------------------------------------------------------
+   public EmptySetException()
+   {
+      super ("The set is empty.");
+   }
+
+   //-----------------------------------------------------------------
+   //-----------------------------------------------------------------
+   public EmptySetException (String message)
+   {
+      super (message);
+   }
+}
