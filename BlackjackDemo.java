cb13442747f559463d0ee9b338c75c361efac91f
Tao Xu <taoxu8888@gmail.com>
2020-10-29 22:04:16 -0400

    swing version of blackjack card game

---
+++ BlackjackDemo.java
@@ -0,0 +1,19 @@
+package cardgame;
+//************************************************************
+//
+//  BlackJackDemo.Java        Authors: Lewis, Chase, Coleman
+//
+//  Provides the driver for a graphical blackjack game
+//
+//************************************************************
+
+public class BlackjackDemo {
+  
+  public static void main(String[] args)
+  {
+
+     BlackjackGUI frame = new BlackjackGUI();
+     frame.display();
+    
+  }
+}
